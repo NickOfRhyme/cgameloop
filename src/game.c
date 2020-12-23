fd7b71d3bf438b728d44bf1d7601cb1f79e6d629
NickOfRhyme <nhwoolrich@gmail.com>
2020-12-23 17:18:08 +0000

    intialise window and renderer

--- src/game.c
+++ src/game.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
+#include "./constants.h"
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
+int initialize_window(void)
+{
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+  {
+    fprintf(stderr, "Error intialising SDL.\n") return FALSE;
+  }
+
+  window = SDL_CreateWindow(
+      NULL,
+      SDL_WINDOWPOS_CENTERED,
+      SDL_WINDOWPOS_CENTERED,
+      WINDOW_WIDTH,
+      WINDOW_HEIGHT,
+      SDL_WINDOW_BORDERLESS);
+  if (!window)
+  {
+    fprintf(stderr, "Error creating SDL window.\n");
+    return FALSE;
+  }
+
+  renderer = SDL_CreateRenderer(window, -1, 0);
+  if (!renderer)
+  {
+    fprintf(stderr, "Error creating SDL renderer.\n");
+    return FALSE;
+  }
+
+  return TRUE;
+}
 
 int main()
 {
-  printf("running game... \n");
+  initialise_window();
   return 0;
 }
